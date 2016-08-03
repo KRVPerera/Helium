6312d37d4756f9f2828a552cef55f5db1f26e93c
Chathura Widanage <chathura.12@cse.mrt.ac.lk>
2016-08-03 20:55:12 +0530

    Twirl filter breaking down to Expressions

--- tests/halide_tests/srcs_halide/halide_complex_twirl.cpp
+++ tests/halide_tests/srcs_halide/halide_complex_twirl.cpp
@@ -12,10 +12,20 @@ int main(int argc, char **argv) {
 	algo taken from https://github.com/alkacon/alkacon-simapi/blob/master/src-dump/filter/TwirlFilter.java
 	*/   
 
-	output(x,y)=input(
-		cast<uint8_t>(min((input.width()/2)+(sqrt(pow(x-(input.width()/2),2)+pow(y-(input.height()/2),2)))*cos(atan2((y-(input.height()/2)),(x-(input.width()/2)))+(5*(min(input.width()/2,input.height()/2) - sqrt(pow(x-(input.width()/2),2)+pow(y-(input.height()/2),2))))/min(input.width()/2,input.height()/2)),input.width()-1)),
-		cast<uint8_t>(min((input.height()/2)+(sqrt(pow(x-(input.width()/2),2)+pow(y-(input.height()/2),2)))*sin(atan2((y-(input.height()/2)),(x-(input.width()/2)))+(5*(min(input.width()/2,input.height()/2) - sqrt(pow(x-(input.width()/2),2)+pow(y-(input.height()/2),2))))/min(input.width()/2,input.height()/2)),input.height()-1)
-	));
+	Expr centerX=input.width()/2;
+    Expr centerY=input.height()/2;
+    Expr radius=min(centerX,centerY);
+    Expr radiusSqr=pow(radius,2); 
+
+    Expr dx=x-(input.width()/2);
+    Expr dy=y-(input.height()/2);
+    Expr distance=sqrt(pow(dx,2)+pow(dy,2));
+    Expr a=atan2(dy,dx)+(5*(radius - distance))/radius;
+   
+    output(x,y)=input(
+        cast<uint8_t>(min(centerX+distance*cos(a),input.width()-1)),
+        cast<uint8_t>(min(centerY+distance*sin(a),input.height()-1)
+    ));
     
     output.compile_to_file("halide_complex_twirl_gen", input); 
     return 0;
