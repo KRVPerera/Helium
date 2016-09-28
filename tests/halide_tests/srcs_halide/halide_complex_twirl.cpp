a8198e9d7df4aa4a48116b5b7e1bd8d25bc503da
Chathura Widanage <chathurawidanage@gmail.com>
2016-09-28 13:17:32 +0530

    Fix twirl filter for small images

--- tests/halide_tests/srcs_halide/halide_complex_twirl.cpp
+++ tests/halide_tests/srcs_halide/halide_complex_twirl.cpp
@@ -5,12 +5,13 @@ int main(int argc, char **argv) {
     
     ImageParam input(UInt(8), 2);
 
-    Func output("output"),delta("delta"),notes("notes");
+    Func output("output");
     Var x("x"), y("y");
-
-    /*
-	algo taken from https://github.com/alkacon/alkacon-simapi/blob/master/src-dump/filter/TwirlFilter.java
-	*/   
+	
+	Func clamped("clamped");
+	Expr clamped_x = clamp(x, 0, input.width() - 1);
+	Expr clamped_y = clamp(y, 0, input.height() - 1);
+	clamped(x, y) = cast<double>(input(clamped_x, clamped_y));
 
 	Expr centerX=input.width()/2;
     Expr centerY=input.height()/2;
@@ -22,10 +23,10 @@ int main(int argc, char **argv) {
     Expr distance=sqrt(pow(dx,2)+pow(dy,2));
     Expr a=atan2(dy,dx)+(5*(radius - distance))/radius;
    
-    output(x,y)=input(
+    output(x,y)=cast<uint8_t>(clamped(
         cast<uint8_t>(min(centerX+distance*cos(a),input.width()-1)),
         cast<uint8_t>(min(centerY+distance*sin(a),input.height()-1)
-    ));
+    )));
     
     output.compile_to_file("halide_complex_twirl_gen", input); 
     return 0;
